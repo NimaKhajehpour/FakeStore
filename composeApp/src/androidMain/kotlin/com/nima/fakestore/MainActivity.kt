8ccea8edc6f577daed5f24d6230236084f242353
NimaKhajehpour <nima1038khajehpour@gmail.com>
2025-05-29 17:43:35 +0430

    fixed themes

--- composeApp/src/androidMain/kotlin/com/nima/fakestore/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/nima/fakestore/MainActivity.kt
@@ -7,16 +7,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalView
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
-import com.example.compose.AppTheme
+import com.nima.fakestore.ui.theme.AppTheme
 import com.nima.fakestore.navigation.AppNavigation
 
 class MainActivity : ComponentActivity() {
--- composeApp/src/androidMain/kotlin/com/nima/fakestore/ui/theme/Color.kt
+++ composeApp/src/androidMain/kotlin/com/nima/fakestore/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.compose
+package com.nima.fakestore.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val primaryLight = Color(0xFF00673E)
